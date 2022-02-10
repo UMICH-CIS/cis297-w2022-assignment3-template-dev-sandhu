8af7913c917651d1be79179a755d35af8dc5e44d
dev-sandhu <75915145+dev-sandhu@users.noreply.github.com>
2022-02-10 18:45:21 -0500

    Add files via upload

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -77,7 +77,18 @@ class Account
      {
          
           get { return accountBalance; }
-          set { accountBalance = value; }
+          set
+          {
+               if (value > 0)
+               {
+                    accountBalance = value;
+               }
+
+               else
+               {
+                    throw new ArgumentOutOfRangeException("Balance", value, "Must be more than 0.00");
+               }
+          }
           
      }
 
@@ -158,3 +169,53 @@ class CheckingAccount : SavingAccount
      }
 
 }
+
+
+///Test
+/*
+
+public class AccountTest
+{
+     static void Main(string[] args)
+     {
+          // create array of accounts
+          Account[] accounts = new Account[4];
+
+          // initialize array with Accounts
+          accounts[0] = new SavingAccount(25M, .03M);
+          accounts[1] = new CheckingAccount(80M, 1M);
+          accounts[2] = new SavingAccount(200M, .015M);
+          accounts[3] = new CheckingAccount(400M, .5M);
+
+          // loop through array, prompting user for debit and credit amounts
+          for (int i = 0; i < accounts.Length; i++)
+          {
+               Console.WriteLine($"Account {i + 1} balance: {accounts[i].accountBalance:C}");
+
+               Console.Write($"\nEnter an amount to withdraw from Account {i + 1}: ");
+               decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+
+               accounts[i].Debit(withdrawalAmount); // attempt to debit
+
+               Console.Write($"\nEnter an amount to deposit into Account {i + 1}: ");
+               decimal depositAmount = decimal.Parse(Console.ReadLine());
+
+               // credit amount to Account
+               accounts[i].Credit(depositAmount);
+
+               // if Account is a SavingsAccount, calculate and add interest
+               if (accounts[i] is SavingsAccount)
+               {
+                    // downcast
+                    SavingsAccount currentAccount = (SavingsAccount)accounts[i];
+
+                    decimal interestEarned = currentAccount.CalculateInterest();
+                    Console.WriteLine($"Adding {interestEarned:C} interest to Account {i + 1} (a SavingsAccount)");
+                    currentAccount.Credit(interestEarned);
+               }
+          }
+          Console.WriteLine($"\nUpdated Account {i + 1} balance: {accounts[i].Balance:C}\n\n");
+     }
+}
+}
+*/
